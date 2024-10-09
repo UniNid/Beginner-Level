88886702fd8b525603106c14cf7804d52ca9dd6d
UniNid <uniNid21@gmail.com>
2024-10-09 20:45:13 +0530

    name changed

--- my-portfolio/src/Components/GrayPill.jsx
+++
@@ -1,5 +0,0 @@
-const GrayPill=({pillTitle})=>{
-   return <span className="bg-gray-300 rounded-full px-4 text-gray-600 font-bold py-1">{pillTitle}</span>
-}
-
-export default GrayPill
--- my-portfolio/src/Components/Link.jsx
+++
@@ -1,8 +0,0 @@
-const Link=({goto, icon, iconTitle})=>{
-    return <a className="flex gap-1 text-blue-500 text-sm hover:text-blue-900"  href={goto} target="_blank">
-        <span >{icon}</span> 
-        <span>{iconTitle}</span>
-    </a>
-}
-
-export default Link;
